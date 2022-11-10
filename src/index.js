50f49fef492db8bf5e48d7be9eb5591316ecd461
Apichart Saelu <102946181+apc9041@users.noreply.github.com>
2022-11-10 22:47:51 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <BrowserRouter basename='/endpoint-2'>
+  <BrowserRouter basename='/app2'>
     <App/>
   </BrowserRouter>
 </React.StrictMode>
